f4591ea990fd948bccbcab0ae477b8fa329c8175
Pete-the-Geek <32913075+Pete-the-Geek@users.noreply.github.com>
2024-04-03 09:38:26 +0100

    Update bat_ident.py
    Added South Wales

--- bat_ident.py
+++ bat_ident.py
@@ -346,7 +346,7 @@ def analyze_file(item):
 
 def set_analysis_location(kHz = 256):
 
-    if args.area not in ["Bavaria",  "Sweden", "UK", "USA","USA-EAST","USA-WEST"]:
+    if args.area not in ["Bavaria",  "South Wales", "Sweden", "UK", "USA","USA-EAST","USA-WEST"]:
         exit(code="Unknown location option or disabled during classifier improvement.")
     else:
         args.lat = -1
@@ -400,6 +400,15 @@ def set_analysis_location(kHz = 256):
 
         cfg.LABELS = utils.readLines(cfg.LABELS_FILE)
 
+    elif args.area == "South Wales":
+        if args.kHz == 144:
+            cfg.CUSTOM_CLASSIFIER = cfg.BAT_CLASSIFIER_LOCATION + "/BattyBirdNET-SouthWales-144kHz.tflite"
+            cfg.LABELS_FILE = cfg.BAT_CLASSIFIER_LOCATION + "/BattyBirdNET-SouthWales-144kHz_Labels.txt"
+        else:
+            cfg.CUSTOM_CLASSIFIER = cfg.BAT_CLASSIFIER_LOCATION + "/BattyBirdNET-SouthWales-256kHz.tflite"
+            cfg.LABELS_FILE = cfg.BAT_CLASSIFIER_LOCATION + "/BattyBirdNET-SouthWales-256kHz_Labels.txt"
+
+        cfg.LABELS = utils.readLines(cfg.LABELS_FILE)
     elif args.area == "UK":
         if args.kHz == 144:
             cfg.CUSTOM_CLASSIFIER = cfg.BAT_CLASSIFIER_LOCATION + "/BattyBirdNET-UK-144kHz.tflite"
